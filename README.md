8c3f8e2ce5e31c18f11b81712821407bebfbf972
Jason Shi <shuwu.shi@gmail.com>
2016-04-11 23:12:27 +0800

    Spring security - HttpSecurity debug

--- README.md
+++ README.md
@@ -61,15 +61,16 @@ Provide REST API and return json as response
 * Define Model
 
 	public class Greeting{...}
-	* Define Controller 
+	
+* Define Controller 
 
 	@RestController
 	public class GreetingController {
-	private static final String template = "Hello, %s!";
-	private AtomicLong counter = new AtomicLong();
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "King of the world") String name) {
-			return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		private static final String template = "Hello, %s!";
+		private AtomicLong counter = new AtomicLong();
+		@RequestMapping("/greeting")
+		public Greeting greeting(@RequestParam(value = "name", defaultValue = "King of the world") String name) {
+				return new Greeting(counter.incrementAndGet(), String.format(template, name));
 		}
 	}
 			
--- src/main/java/com/jasonshi/sample/config/WebappSecurity.java
+++ src/main/java/com/jasonshi/sample/config/WebappSecurity.java
@@ -2,6 +2,7 @@ package com.jasonshi.sample.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,14 +13,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebappSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().
-				antMatchers("/people", "/people/**").access("hasRole('USER')")
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.permitAll();
-
-		// Or the curl or postman cannot get/post data
-		http.csrf().disable();
-
+		http
+		// disable csrf
+		.csrf().disable()
+		
+		.authorizeRequests()
+		.antMatchers("/", "/home").permitAll()
+		
+		// people api
+		.antMatchers("/people", "/people/**").authenticated()
+		
+		.anyRequest().authenticated()
+		
+		// login
+		.and()
+			.formLogin().loginPage("/login").permitAll()
+			
+		// logout
+		.and().logout().permitAll()
+		
+		// http basic
+		.and().httpBasic();
 	}
 
 	@Autowired
